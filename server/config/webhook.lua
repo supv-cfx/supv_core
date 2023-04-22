099e647b84f36b1519d47c0ddd166ae9f4f341e1
SUP2Ak <31973315+SUP2Ak@users.noreply.github.com>
2023-04-22 16:59:45 +0000

    feat: (server/webhook.lua) supv.webhook function

--- server/config/webhook.lua
+++ server/config/webhook.lua
@@ -6,5 +6,12 @@ return {
         ['supv_tebex'] = ''
     },
 
-    default = {}
+    default = {
+        date_format = 'letter', -- letter or numeric
+        color = 7055103,
+        foot_icon = 'https://avatars.githubusercontent.com/u/95303960?s=280&v=4',
+        avatar = "https://avatars.githubusercontent.com/u/95303960?s=280&v=4"
+    },
+
+    played_from = 'server' -- shared or server
 }
