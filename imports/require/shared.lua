9d10a63bc3e2c17a30ac9ae0d1b3987ec52db3df
SUP2Ak <31973315+SUP2Ak@users.noreply.github.com>
2023-04-22 12:52:21 +0000

    add module: (imports/require/shared.lua) overwrite require function

---
+++ imports/require/shared.lua
@@ -0,0 +1,28 @@
+local moduleLoaded = {}
+
+local function load_module(path)
+    if moduleLoaded[path] then
+        return moduleLoaded[path]
+    end
+
+    local module_path = ("%s.lua"):format(path)
+    local module_file = LoadResourceFile(GetCurrentResourceName(), module_path)
+    if not module_file then
+        error("Impossible de chargé le module : "..path)
+    end
+
+    moduleLoaded[path] = load(module_file)()
+    return moduleLoaded[path]
+end
+
+local function call_module(path)
+    local module = load_module(path)
+    if not module then
+        return error("Le module n'a pas charger : "..path)
+    end
+    return module
+end
+
+return {
+    load = call_module
+}
