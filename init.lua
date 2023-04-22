2a61e8a6dbd8327fff30b6aaff357fc68eb611b2
SUP2Ak <31973315+SUP2Ak@users.noreply.github.com>
2023-04-22 12:50:07 +0000

    add init.lua

---
+++ init.lua
@@ -0,0 +1,46 @@
+local supv_core <const> = 'supv_core'
+local LoadResourceFile <const>, IsDuplicityVersion <const> = LoadResourceFile, IsDuplicityVersion
+local service <const> = (IsDuplicityVersion() and 'server') or 'client'
+local GetGameName <const> = GetGameName
+
+local function load_module(self, index)
+    local dir <const> = ("imports/%s"):format(index)
+    local chunk <const> = LoadResourceFile(supv_core, ('%s/%s.lua'):format(dir, service))
+    local shared <const> = LoadResourceFile(supv_core, ('%s/shared.lua'):format(dir))
+    local func, err
+    
+    if chunk or shared then
+        if shared then
+            func, err = load(shared, ('@@%s/%s/%s'):format(supv_core, index, 'shared'))
+        else
+            func, err = load(chunk, ('@@%s/%s/%s'):format(supv_core, index, service))
+        end
+
+        if not func or err then return error(("Erreur pendant le chargement du module\n- Provenant de : %s\n- Modules : %s\n- Service : %s\n - Erreur : %s"):format(dir, index, service, err), 3) end
+
+        local result = func()
+        self[index] = result
+        return self[index]
+    end
+end
+
+local function call_module(self, index, ...)
+    local module = rawget(self, index)
+
+    if not module then
+        module = load_module(self, index)
+        return module
+    end
+end
+
+supv = setmetatable({
+    service = service, 
+    name = GetCurrentResourceName(),
+    game = GetGameName()
+}, {
+    __index = call_module, 
+    __call = call_module, 
+    __newindex = function(self, name, func)
+    rawset(self, name, func)
+    exports(name, func)
+end})
