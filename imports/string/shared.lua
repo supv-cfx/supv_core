fc5b1bdfc1add5b0eccea164a89dc896a9d0787f
SUP2Ak <31973315+SUP2Ak@users.noreply.github.com>
2023-04-22 17:08:21 +0000

    feat: (imports/string/shared.lua) string module

---
+++ imports/string/shared.lua
@@ -0,0 +1,42 @@
+local string <const>, math <const> = string, math
+
+--- supv.string.starts
+---@param str string
+---@param start number
+---@return boolean
+local function Starts(str, start)
+    return string.sub(str, 1, string.len(start)) == start
+end
+
+--- supv.string.firstToUpper
+---@param str string
+---@return string
+local function FirstToUpper(str)
+    str = str:lower()
+    return str:gsub("^%l", string.upper)
+end
+
+--- supv.string.random
+---@param length number
+---@param s? boolean
+---@return string
+local function RandomString(length, s)
+    local l = length or 70
+    local upperCase, lowerCase, numbers, symbols = "ABCDEFGHIJKLMNOPQRSTUVWXYZ", "abcdefghijklmnopqrstuvwxyz", "0123456789"
+    if s then symbols = "!@#$%&()*+-,./:;<=>?^[]{}" else symbols = nil end
+    local characterSet = symbols and upperCase..lowerCase..numbers..symbols or upperCase..lowerCase..numbers
+    local output = ""
+
+    for i = 1, l do
+        local random = math.random(#characterSet)
+        output = output..characterSet:sub(random, random)
+    end
+    
+    return output
+end
+
+return {
+    starts = Starts,
+    random = RandomString,
+    firstToUpper = FirstToUpper
+}
