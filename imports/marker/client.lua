18a20eed427f93eeeafc9c505922c3c1f2d81d09
SUP2Ak <cormier.wesley@gmail.com>
2023-04-25 03:12:18 +0200

    tweak: (imports/marker.client) data.visible -> options.visible

--- imports/marker/client.lua
+++ imports/marker/client.lua
@@ -1,9 +1,4 @@
-
-local DrawMarker <const> = DrawMarker
-local default <const> = supv.config.marker
-
----@class DataProps
----@field visible? boolean-true
+---@class MarkerDataProps
 ---@field id? integer
 ---@field color1? table
 ---@field color2? table
@@ -20,17 +15,21 @@ local default <const> = supv.config.marker
 ---@field op? integer
 ---@field rotate? boolean
 
----@class OptionProps
+---@class MarkerOptionProps
+---@field visible? boolean-true
 ---@field double? boolean
 ---@field insible? boolean
 
+local DrawMarker <const>, HasStreamedTextureDictLoaded <const>, RequestStreamedTextureDict <const> = DrawMarker, HasStreamedTextureDictLoaded, RequestStreamedTextureDict
+local default <const> = supv.config.marker
+
 --- supv.marker.draw
 ---@param coords vec3
----@param data? DataProps
----@param options? OptionProps
+---@param data? MarkerDataProps
+---@param options? MarkerOptionProps
 local function Marker(coords, data, options)
-    local p1, p2 = {}, options?.double and {} or default.double
-    p1.visible = data[1]?.visible or true
+    local p1, p2, visible = {}, options?.double and {} or default.double, options?.visible or true
+    --p1.visible = data[1]?.visible or true ---@deprecated
     p1.id = data[1]?.id or default[1].id
     p1.color1 = data[1]?.color1 or default[1].color1
     p1.color2 = data[1]?.color2 or default[1].color2
@@ -48,7 +47,7 @@ local function Marker(coords, data, options)
     p1.rotate = data[1]?.rotate or default[1].rotate
 
     if p2 then
-        p2.visible = data[2]?.visible or true
+        --p2.visible = data[2]?.visible or true ---@deprecated
         p2.id = data[2]?.id or default[2].id
         p2.color1 = data[2]?.color1 or default[2].color1
         p2.color2 = data[2]?.color2 or default[2].color2
@@ -66,25 +65,11 @@ local function Marker(coords, data, options)
         p2.rotate = data[2]?.rotate or default[2].rotate
     end
 
-    local z1, z2, color1, color2 = (p1.z + coords.z), p2 and (p2.z + coords.z)
-
-    if options?.inside then
-        color1 = p1.color1
-        color2 = p2 and p2.color1
-    else
-        color1 = p1.color2
-        color2 = p2 and p2.color2
-    end
-
     if p1.textureDict and p1.textureName and not HasStreamedTextureDictLoaded(p1.textureDict) then
         RequestStreamedTextureDict(p1.textureDict, true)
         while not HasStreamedTextureDictLoaded(p1.textureDict) do
             Wait(1)
         end
-    end
-
-    if p1.visible then
-        DrawMarker(p1.id, coords.x, coords.y, z1, p1.dir.x, p1.dir.y, p1.dir.z, p1.rot.x, p1.rot.y, p1.rot.z, p1.scale.x, p1.scale.y, p1.scale.z, color1[1], color1[2], color1[3], color1[4], p1.updown, p1.faceToCam, p1.p19, p1.rotate, p1.textureDict, p1.textureName, p1.drawOnEnts)
     end
 
     if p2 then
@@ -94,8 +79,21 @@ local function Marker(coords, data, options)
                 Wait(1)
             end
         end
+    end
 
-        if p2.visible then
+    if options?.inside then
+        color1 = p1.color1
+        color2 = p2 and p2.color1
+    else
+        color1 = p1.color2
+        color2 = p2 and p2.color2
+    end
+
+    local z1, z2, color1, color2 = (p1.z + coords.z), p2 and (p2.z + coords.z)
+
+    if visible then
+        DrawMarker(p1.id, coords.x, coords.y, z1, p1.dir.x, p1.dir.y, p1.dir.z, p1.rot.x, p1.rot.y, p1.rot.z, p1.scale.x, p1.scale.y, p1.scale.z, color1[1], color1[2], color1[3], color1[4], p1.updown, p1.faceToCam, p1.p19, p1.rotate, p1.textureDict, p1.textureName, p1.drawOnEnts)
+        if p2 then
             DrawMarker(p2.id, coords.x, coords.y, z2, p2.dir.x, p2.dir.y, p2.dir.z, p2.rot.x, p2.rot.y, p2.rot.z, p2.scale.x, p2.scale.y, p2.scale.z, color2[1], color2[2], color2[3], color2[4], p2.updown, p2.faceToCam, p2.p19, p2.rotate, p2.textureDict, p2.textureName, p2.drawOnEnts)
         end
     end
