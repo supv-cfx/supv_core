8501ae339dde774ea6d165294d603c6d989e7d54
SUP2Ak <cormier.wesley@gmail.com>
2023-04-26 22:49:16 +0200

    feat: exports supv (load)

---
+++ client/exports.lua
@@ -0,0 +1,10 @@
+exports('load', function()
+    local file = 'obj.lua'
+    local import = LoadResourceFile(supv.name, file)
+    local func, err = load(import, ('@@%s/%s'):format(supv.name, file))
+    if not func or err then
+        return error(err or ("unable to load module '%s'"):format(file), 3)
+    end
+
+    return func()
+end)
