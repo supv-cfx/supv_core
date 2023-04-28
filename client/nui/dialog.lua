d67bacabe3db3fc203aa219c74d21a3827e5e7d3
SUP2Ak <cormier.wesley@gmail.com>
2023-04-28 14:34:56 +0200

    feat: (client/dialog.lua) supv.createDialog

---
+++ client/nui/dialog.lua
@@ -0,0 +1,32 @@
+local p, nui <const> = nil, require 'client.modules.nui'
+
+---@param data boolean
+nui.RegisterReactCallback('supv:dialog:closed', function(data, cb)
+    if p then p:resolve(data) end p = nil
+    cb(1)
+end, true)
+
+
+---@class DialogDataProps
+---@field title string
+---@field subtitle? string
+---@field description string
+
+--- supv.createDialog
+---@param data DialogDataProps
+---@return boolean
+function supv.createDialog(data)
+    if type(data) ~= 'table' then return end
+    if not data.title or not data.description then return end
+    if p then return end
+
+    nui.SendReactMessage(true, {
+        action = 'supv:dialog:create',
+        data = data
+    }, {
+        focus = true
+    })
+
+    p = promise.new()
+    return supv.await(p)
+end
--- client/nui/unix.lua
+++ client/nui/unix.lua
@@ -1,10 +1,16 @@
 local p, nui <const> = nil, require 'client.modules.nui'
 
+---@param data string
 nui.RegisterReactCallback('supv:convert:return-unix', function(data, cb)
     if p then p:resolve(data) end p = nil
     cb(1)
 end, true)
 
+
+--- supv.convertUnixTime
+---@param unix number
+---@param date? string
+---@return string
 function supv.convertUnixTime(unix, date)
     if type(unix) ~= 'number' then return end
     if p then return end
