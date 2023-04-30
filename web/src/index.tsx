aa30a4ca04bd023725edade958c7381a7de41d7a
SUP2Ak <cormier.wesley@gmail.com>
2023-04-30 10:58:10 +0200

    feat/refactor: (web) index.tsx
    - feat:
        - import (library: fas, far, fabs)
    - refactor :
        - root env

--- web/src/index.tsx
+++ web/src/index.tsx
@@ -1,15 +1,33 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-//import {VisibilityProvider} from './providers/VisibilityProvider';
 import { MantineProvider } from '@mantine/core';
+import { fas } from '@fortawesome/free-solid-svg-icons';
+import { far } from '@fortawesome/free-regular-svg-icons';
+import { fab } from '@fortawesome/free-brands-svg-icons';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { isEnvBrowser } from './utils/misc';
+import './index.css';
 
-ReactDOM.render(
+//import {VisibilityProvider} from './providers/VisibilityProvider';
+
+library.add(fas, far, fab);
+
+if (isEnvBrowser()) {
+  const root = document.getElementById('root');
+
+  root!.style.backgroundImage = 'url("https://i.imgur.com/3pzRj9n.png")';
+  root!.style.backgroundSize = 'cover';
+  root!.style.backgroundRepeat = 'no-repeat';
+  root!.style.backgroundPosition = 'center';
+}
+
+const root = document.getElementById('root');
+ReactDOM.createRoot(root!).render(
   <React.StrictMode>
     <MantineProvider theme={{ colorScheme: 'dark' }} withGlobalStyles withNormalizeCSS>
       <App />
     </MantineProvider>  
   </React.StrictMode>,
-  document.getElementById('root')
 );
