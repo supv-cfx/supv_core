c4083a463356f365ea1cbdb103cddaaf1ff9ebe1
SUP2Ak <cormier.wesley@gmail.com>
2023-04-30 21:35:49 +0200

    refactor: (client/notify) change event register

--- client/notify.lua
+++ client/notify.lua
@@ -4,9 +4,7 @@ local notify = supv.require.get[('client.modules.notify_%s'):format(notif.type)]
 function supv.notify(...)
     if not notify then
         notify = require(('client.modules.notify_%s'):format(notif.type))
-        RegisterNetEvent('supv_core:notify', function(...)
-            notify(...)
-        end)
+        supv.eventRegister('supv_core:notify', notify)
     end
     notify(...)
 end
