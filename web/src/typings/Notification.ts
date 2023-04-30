077f2e937a8f1015839ad6b1309bc8d88a9199e9
SUP2Ak <cormier.wesley@gmail.com>
2023-04-30 21:06:34 +0200

    refactor: (web) typings/Notification.ts

--- web/src/typings/Notification.ts
+++ web/src/typings/Notification.ts
@@ -1,18 +1,15 @@
-import React from 'react';
+//import React from 'react';
 import { ToastPosition } from 'react-hot-toast';
 import { IconProp } from '@fortawesome/fontawesome-svg-core';
 
 export interface NotificationProps {
-  style?: React.CSSProperties;
-  description?: string;
   title?: string;
-  duration?: number;
-  icon?: IconProp;
-  iconColor?: string;
-  position?: ToastPosition;
   id?: number | string;
-  type?: string;
-
-  color?: string;
-  closable?: boolean;
+  description?: string; 
+  type?: string; 
+  duration?: number; // default: 3000
+  icon?: IconProp; // default: check on type
+  position?: ToastPosition; // default: top-right
+  color?: string; // default: check on type
+  closable?: boolean; // default: false (not semi-implementation)
 }
