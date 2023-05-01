df5e3b5929020649815b74cc1e1169786c8493c3
SUP2Ak <cormier.wesley@gmail.com>
2023-05-01 05:23:21 +0200

    chore: (web) Réorganisation

---
+++ web/src/typings/index.ts
@@ -0,0 +1,8 @@
+
+export * from './ConvertUnix';
+export * from './Notification';
+export * from './ConvertUnix';
+export * from './InputDate';
+
+// configuration
+export * from './config/notifications';
