4d64cb9038fde9ec016a8055b4520c95c9a20a2d
SUP2Ak <cormier.wesley@gmail.com>
2023-05-01 05:25:29 +0200

    refactor: (web) ConfigProvider (use default ConfigDev & Typing Notification)

--- web/src/providers/ConfigProvider.tsx
+++ web/src/providers/ConfigProvider.tsx
@@ -1,16 +1,17 @@
 import { Context, createContext, useContext, useEffect, useState } from 'react';
 import { MantineColor } from '@mantine/core';
 import { fetchNui } from '../utils/fetchNui';
+import { 
+  NotificationConfigProviderProps
+} from '../typings';
+import {
+  NotificationConfigDev
+} from '../dev/config';
 
 interface Config {
   primaryColor: MantineColor;
   primaryShade: 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9;
-  notificationStyles: {
-    container: React.CSSProperties;
-    title: React.CSSProperties;
-    description: React.CSSProperties;
-    descriptionOnly: React.CSSProperties;
-  }
+  notificationStyles: NotificationConfigProviderProps;
 }
 
 interface ConfigCtxValue {
@@ -24,34 +25,7 @@ const ConfigProvider: React.FC<{ children: React.ReactNode }> = ({ children }) =
   const [config, setConfig] = useState<Config>({
     primaryColor: 'blue',
     primaryShade: 6,
-    notificationStyles: {
-      container: {
-        width: 'fit-content',
-        //width: 300,
-        maxWidth: 400,
-        minWidth: 300,
-        height: 'fit-content',
-        backgroundColor: 'dark.4',
-        fontFamily: 'Ubuntu',
-      },
-      title: {
-        fontWeight: 500,
-        lineHeight: 'normal',
-        color: 'gray.6',
-      },
-      description: {
-        fontSize: 12,
-        color: 'gray.4',
-        fontFamily: 'Ubuntu',
-        lineHeight: 'normal',
-      },
-      descriptionOnly: {
-        fontSize: 14,
-        color: 'gray.2',
-        fontFamily: 'Ubuntu',
-        lineHeight: 'normal',
-      },
-    }
+    notificationStyles: NotificationConfigDev,
   });
 
   useEffect(() => {
