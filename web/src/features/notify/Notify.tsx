b8ffd80d4c37a85c687ded7ef3e24e718bc3fe0b
SUP2Ak <cormier.wesley@gmail.com>
2023-05-01 05:27:01 +0200

    refactor: (web) Notify.tsx - Remove import unused - Clear code

--- web/src/features/notify/Notify.tsx
+++ web/src/features/notify/Notify.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { toast, Toaster } from 'react-hot-toast';
 import ReactMarkdown from 'react-markdown';
-import { Transition, createStyles, Notification, keyframes } from '@mantine/core';
+import { Transition, createStyles, Notification } from '@mantine/core';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { useNuiEvent } from '../../hooks/useNuiEvent';
 import type { NotificationProps } from '../../typings/Notification';
@@ -17,14 +17,14 @@ const scaleY = {
 
 const slideDown = { // animation compatible all the way
     in: {
-      opacity: 1,
-      transform: 'translateY(0)',
-      transitionDuration: '0.5s',
+        opacity: 1,
+        transform: 'translateY(0)',
+        transitionDuration: '0.5s',
     },
     out: {
-      opacity: 0,
-      transform: 'translateY(-10px)',
-      transitionDuration: '0.5s',
+        opacity: 0,
+        transform: 'translateY(-10px)',
+        transitionDuration: '0.5s',
     },
     common: {},
     transitionProperty: 'transform, opacity',
@@ -32,72 +32,46 @@ const slideDown = { // animation compatible all the way
 
 const pendouillage = { // pendouillage très bien a droite et surtout en bas
     in: {
-      opacity: 1,
-      transform: 'rotate(0deg) translateY(0)',
-      transitionTimingFunction: 'cubic-bezier(0.18, 0.89, 0.32, 1.28)',
+        opacity: 1,
+        transform: 'rotate(0deg) translateY(0)',
+        transitionTimingFunction: 'cubic-bezier(0.18, 0.89, 0.32, 1.28)',
     },
     out: {
-      opacity: 0,
-      transform: 'rotate(-90deg) translateY(-10px)',
-      transitionTimingFunction: 'cubic-bezier(0.6, -0.28, 0.74, 0.05)',
+        opacity: 0,
+        transform: 'rotate(-90deg) translateY(-10px)',
+        transitionTimingFunction: 'cubic-bezier(0.6, -0.28, 0.74, 0.05)',
     },
     common: {
-      transformOrigin: 'top right',
+        transformOrigin: 'top right',
     },
     transitionProperty: 'transform, opacity',
 };
 
 const toastTransition = {
     in: {
-      opacity: 1,
-      transform: 'scale(1)',
-      transitionDuration: '1.0s',
+        opacity: 1,
+        transform: 'scale(1)',
+        transitionDuration: '1.0s',
     },
     out: {
-      opacity: 0,
-      transform: 'scale(0)',
-      transitionDuration: '1.0s',
+        opacity: 0,
+        transform: 'scale(0)',
+        transitionDuration: '1.0s',
     },
     common: {
     },
     transitionProperty: 'transform, opacity',
 };
-  
-  
+
 const Notifications: React.FC = () => {
 
     const { config } = useConfig();
-    
-    const useSyles = createStyles((theme) => ({
-        container: {
-            width: config.notificationStyles.container.width,
-            //width: 300,
-            maxWidth: config.notificationStyles.container.maxWidth,
-            minWidth: config.notificationStyles.container.minWidth,
-            height: 'fit-content',
-            backgroundColor: 'dark.4',
-            fontFamily: 'Ubuntu',
-        },
-        title: {
-            fontWeight: 500,
-            lineHeight: 'normal',
-            color: 'gray.6',
-        },
-        description: {
-            fontSize: 12,
-            color: 'gray.4',
-            fontFamily: 'Ubuntu',
-            lineHeight: 'normal',
-        },
-        descriptionOnly: {
-            fontSize: 14,
-            color: 'gray.2',
-            fontFamily: 'Ubuntu',
-            lineHeight: 'normal',
-        },
+
+    const useStyles = createStyles((theme) => ({
+        ...config.notificationStyles
     }));
 
-    const { classes } = useSyles();
+    const { classes } = useStyles();
 
     const onRemoveQueue = async () => {
         await new Promise((resolve) => setTimeout(resolve, 200));
@@ -111,7 +85,6 @@ const Notifications: React.FC = () => {
         if (!data.icon && data.type !== 'loading' && data.type) {
             data.icon = data.type === 'error' ? 'xmark' : data.type === 'success' ? 'check' : data.type === 'warning' ? 'exclamation' : 'info';
         }
-
         let description: string = data.description ? data.description.replace('\n', '  \n  ') : '';
 
         const Notify = (data: NotificationProps) => {
