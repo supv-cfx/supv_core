e37778cecbdce0eadf5ed7940d3601d29f96b91e
SUP2Ak <cormier.wesley@gmail.com>
2023-05-01 04:18:59 +0200

    tweak|feat: (client/nui) - tweak remvoe print - feat : init config loaded nui

--- client/nui/config.lua
+++ client/nui/config.lua
@@ -2,7 +2,6 @@ local notify <const> = require 'client.config.notify'
 local nui <const> = require 'client.modules.nui'
 
 if notify.type == 'react' then
-    print(json.decode(json.encode(notify.react.notificationStyles, {indent = true})))
     nui.RegisterReactCallback('supv:react:getConfig', function(data, cb)
         cb({
             notificationStyles = json.decode(notify.react.notificationStyles),
