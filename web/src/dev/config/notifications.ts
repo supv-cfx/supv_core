7423e3795b8c180b8f8b17d6877e8df469c69329
SUP2Ak <cormier.wesley@gmail.com>
2023-05-01 05:24:01 +0200

    feat: (web) Notification config dev

---
+++ web/src/dev/config/notifications.ts
@@ -0,0 +1,27 @@
+export const NotificationConfigDev = {
+    container: {
+        width: 300,
+        maxWidth: 400,
+        minWidth: 300,
+        height: 'fit-content',
+        backgroundColor: 'dark.4',
+        fontFamily: 'Ubuntu',
+    },
+    title: {
+        fontWeight: 500,
+        lineHeight: 'normal',
+        color: 'gray.6',
+    },
+    description: {
+        fontSize: 12,
+        color: 'gray.4',
+        fontFamily: 'Ubuntu',
+        lineHeight: 'normal',
+    },
+    descriptionOnly: {
+        fontSize: 14,
+        color: 'gray.2',
+        fontFamily: 'Ubuntu',
+        lineHeight: 'normal',
+    },
+}
