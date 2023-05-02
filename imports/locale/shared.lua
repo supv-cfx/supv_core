4f3fb3e986473ac00b0262ba4ecb3d6b88b7f598
SUP2Ak <cormier.wesley@gmail.com>
2023-05-02 06:25:59 +0200

    refactor: (imports/loacle) can load extern translation

--- imports/locale/shared.lua
+++ imports/locale/shared.lua
@@ -1,4 +1,4 @@
-local data = {}
+local data = {} ---@type { [string]: string}
 
 ---@param str string
 ---@param ... any
@@ -15,26 +15,83 @@ function translate(str, ...)
     return str
 end
 
+--- supv.locale.init
 local function Initialize()
     local env = supv.json.load(('locales/%s'):format(supv.lang))
     if not env then
        return warn(("Impossible de chargé locales/%s.json dans l'environnement : %s"):format(supv.lang, supv.env))
     end
 
-    for k,v in pairs(env) do
-        if type(v) == 'string' then
+    for k, v in pairs(env) do
+        if not data[k] then
+            if type(v) == 'string' then
+                for var in v:gmatch('${[%w%s%p]-}') do -- credit: ox_lib <https://github.com/overextended/ox_lib/blob/master/imports/locale/shared.lua>
+                    local locale = env[var:sub(3, -2)]
+
+                    if locale then
+                        locale = locale:gsub('%%', '%%%%')
+                        v = v:gsub(var, locale)
+                    end
+                end
+            end
             data[k] = v
         end
     end
 end
 
+--- supv.locale.get
 ---@param key? string
 ---@return table|string
 local function GetInitialized(key)
     return data[key] or data
 end
 
+--- supv.locale.load ( example : supv.locale.load('es_extended', 'locales.en', 'lua') | supv.locale.load('supv_core', 'locales.en', 'json') )
+---@param resource string
+---@param path string
+---@param ext string
+local function LoadExternal(resource, path, ext)
+    local filePath, folder = path, resource
+    filePath = filePath:gsub('%.', '/')
+    if ext == 'json' then goto JSON end
+    local import = LoadResourceFile(folder, filePath)
+    local func, err = load(import, ('@@%s/%s/%s.lua'):format(folder, filePath))
+    if not func or err then
+        return error(err or ("unable to load module '%s/%s.lua'"):format(folder, filePath), 3)
+    end
+
+    local result = func()
+
+    if ext == 'json' then
+        ::JSON::
+        filePath = filePath:gsub('%.json', '')
+        result = supv.json.load(filePath, folder)
+    end
+
+    if type(result) ~= 'table' then
+        return error(("Le fichier '%s/%s' ne retourne pas une table"):format(folder, filePath), 2)
+    end
+
+    for k, v in pairs(result) do
+        if not data[k] then
+            if type(v) == 'string' then
+                for var in v:gmatch('${[%w%s%p]-}') do -- credit: ox_lib <https://github.com/overextended/ox_lib/blob/master/imports/locale/shared.lua>
+                    local locale = result[var:sub(3, -2)]
+
+                    if locale then
+                        locale = locale:gsub('%%', '%%%%')
+                        v = v:gsub(var, locale)
+                    end
+                end
+            end
+
+            data[k] = v
+        end
+    end
+end
+
 return {
     init = Initialize,
-    get = GetInitialized
+    get = GetInitialized,
+    load = LoadExternal,
 }
