75875c9137185ccd47c3cfaafa1987e1550914a3
SUP2Ak <cormier.wesley@gmail.com>
2023-05-05 01:37:20 +0200

    feat: (web) Switch side devTool

--- web/src/dev/DevEnv.tsx
+++ web/src/dev/DevEnv.tsx
@@ -1,18 +1,18 @@
 import React, { useState } from 'react';
 import { ActionIcon, Tooltip, Drawer, Stack, Divider, Button } from '@mantine/core';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faWrench } from '@fortawesome/free-solid-svg-icons';
+import { faWrench, faArrowLeft, faArrowRight } from '@fortawesome/free-solid-svg-icons';
 
 import { debugDialog } from './debug/dialog';
 import { debugNotification } from './debug/notifcation';
 
 const DevTool: React.FC = () => {
-
     const [opened, setOpened] = useState<boolean>(false);
+    const [side, setSide] = useState<'left' | 'right'>('right');
 
     return (
         <>
-            <Tooltip label="Dev Tool" position="left">
+            <Tooltip label="Open [supv] Tool" position={'right'}>
                 <ActionIcon
                     variant="outline"
                     color="gray"
@@ -23,8 +23,13 @@ const DevTool: React.FC = () => {
                 </ActionIcon>
             </Tooltip>
 
-            <Drawer opened={opened} onClose={() => setOpened(false)} title="tool dev" padding="md" size={300}>
+            <Drawer opened={opened} onClose={() => setOpened(false)} title="supv core tool dev" padding="md" size={300}
+                position={side === 'left' ? 'right' : 'left'}
+            >
                 <Stack spacing="md">
+                    <Button rightIcon={<FontAwesomeIcon icon={faArrowRight}/>} leftIcon={<FontAwesomeIcon icon={faArrowLeft}/>} 
+                        variant="outline" color="orange" size='xs' fullWidth onClick={() => setSide(side === 'left' ? 'right' : 'left')}
+                    >Side</Button>
                     <Divider />
                     <Button variant="outline" color="blue" fullWidth onClick={() => {debugDialog(); setOpened(false)}}>Dialog</Button>
                     <Button variant='outline' color='red' fullWidth onClick={() => debugNotification()}>Notication</Button>
