7e04568f53253d99767c49ad28f3dfcfed4bf89e
SUP2Ak <cormier.wesley@gmail.com>
2023-05-04 23:12:25 +0200

    refactor: (server.events) remove while token + (wip cooldown)

--- server/events.lua
+++ server/events.lua
@@ -1,36 +1,38 @@
 local RegisterNetEvent <const>, AddEventHandler <const>, TriggerClientEvent <const>, TriggerEvent <const> = RegisterNetEvent, AddEventHandler, TriggerClientEvent, TriggerEvent
-
 local token <const> = require 'server.modules.tokenizer'
+
+---@todo: Need some time to imagine process of cooldowns for not change it in future
+-- local timerSource, TimerGlobal, GetGameTimer <const> = {}, {}, GetGameTimer 
+
+supv.callback.register('event', function(source)
+    return token
+end)
 
 --- supv.eventRegister @ RegisterNetEvent
 ---@param name string
 ---@param cb? function
+---@param cooldown CooldDownProps
 function supv.eventRegister(name, cb)
     if type(name) ~= 'string' then return end
     if cb and type(cb) ~= 'function' then return end
 
-    while not token do
-        Wait(500)
-    end
-
     name = ("__%s__:%s:%s:%s"):format('supv', token, supv.service,name)
-
     RegisterNetEvent(name, cb)
 end
 
 --- supv.eventHandler @ AddEventHandler
 ---@param name string
 ---@param cb function
-function supv.eventHandler(name, cb)
+---@param cooldown CooldDownProps
+function supv.eventHandler(name, cb, cooldown)
     if type(name) ~= 'string' then return end
     if cb and type(cb) ~= 'function' then return end
 
-    while not token do
-        Wait(500)
+    if type(cooldown) == 'table' then
+        RegisterCooldown(name, cooldown.time, cooldown.type)
     end
 
     name = ("__%s__:%s:%s:%s"):format('supv', token, supv.service,name)
-    
     AddEventHandler(name, cb)
 end
 
@@ -40,12 +42,7 @@ end
 function supv.trigger(name, ...)
     if type(name) ~= 'string' then return end
 
-    while not token do
-        Wait(500)
-    end
-
     name = ("__%s__:%s:%s:%s"):format('supv', token, supv.service, name)
-    
     TriggerEvent(name, ...)
 end
 
@@ -55,15 +52,6 @@ end
 function supv.triggerClient(name, ...)
     if type(name) ~= 'string' then return end
 
-    while not token do
-        Wait(500)
-    end
-
     name = ("__%s__:%s:%s:%s"):format('supv', token, 'client', name)
-    
     TriggerClientEvent(name, ...)
 end
-
-supv.callback.register('event', function(source)
-    return token
-end)
