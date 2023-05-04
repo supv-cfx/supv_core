0f87c8cc7fc920f03d4cef58245957301e995e87
SUP2Ak <cormier.wesley@gmail.com>
2023-05-04 23:11:32 +0200

    refactor[feat: (client.events) Cooldown + Token
    - refactor
    Awaiting token before finish execute export
    
    - feat
    Add cooldown in trigger
    Register cooldown on RegisterEvent (cb) & AddEventHandler

--- client/events.lua
+++ client/events.lua
@@ -1,40 +1,85 @@
 local RegisterNetEvent <const>, AddEventHandler <const>, TriggerServerEvent <const>, TriggerEvent <const>, token = RegisterNetEvent, AddEventHandler, TriggerServerEvent, TriggerEvent
+local timers, GetGameTimer <const> = {}, GetGameTimer
+
+local function GetToken()
+    return supv.callback.sync('event')
+end
+
+---@param name string
+---@return table|false
+local function IsEventCooldown(name)
+    if timers[name] then
+        return timers[name]
+    end
+    return false
+end
+
+---@return boolean
+local function GetCooldown(self)
+    if not self then return end
+    local time = GetGameTimer()
+    if (time - self.time) < self.cooldown then
+        return true
+    end
+    self.time = time
+    return false
+end
+
+---@param name string
+---@param timer number
+local function RegisterCooldown(name, timer)
+    local self = {}
+    
+    self.time = GetGameTimer()
+    self.cooldown = timer
+    self.onCooldown = GetCooldown
+
+    timers[name] = self
+end
 
 Citizen.CreateThreadNow(function()
-    token = supv.callback.sync('event')
+    token = GetToken()
 end)
 
 --- supv.eventHandler @ RegisterNetEvent
 ---@param name string
 ---@param cb? function
-function supv.eventRegister(name, cb)
+---@param cooldown? number
+function supv.eventRegister(name, cb, cooldown)
     if type(name) ~= 'string' then return end
     if cb and type(cb) ~= 'function' then return end
 
-    CreateThread(function()
-        while not token do
-            Wait(500)
-        end
+    if type(cooldown) == 'number' then
+        RegisterCooldown(name, cooldown)
+    end
 
-        name = ("__%s__:%s:%s:%s"):format('supv', token, supv.service,name)
-        
-        RegisterNetEvent(name, cb)
-    end)
+    if not token then
+        token = GetToken()
+        if not token then return warn(("This RegisterEvent '%s' not registered"):format(name)) end
+    end
+
+    name = ("__%s__:%s:%s:%s"):format('supv', token, supv.service,name)
+    RegisterNetEvent(name, cb)
 end
 
 --- supv.eventHandler @ AddEventHandler
 ---@param name string
 ---@param cb function
-function supv.eventHandler(name, cb)
+---@param cooldown? number
+function supv.eventHandler(name, cb, cooldown)
     if type(name) ~= 'string' then return end
     if cb and type(cb) ~= 'function' then return end
 
-    while not token do
-        Wait(500)
+    if type(cooldown) == 'number' then
+        RegisterCooldown(name, cooldown)
     end
 
-    name = ("__%s__:%s:%s:%s"):format('supv', token, supv.service,name)
-    
+    if not token then
+        token = GetToken()
+        if not token then return warn(("This AddEventHandler '%s' not registered"):format(name)) end
+    end
+
+    name = ("__%s__:%s:%s:%s"):format('supv', token, supv.service,name) 
     AddEventHandler(name, cb)
 end
 
@@ -44,12 +89,15 @@ end
 function supv.trigger(name, ...)
     if type(name) ~= 'string' then return end
 
-    while not token do
-        Wait(500)
+    local event = IsEventCooldown(name)
+    if event and event:onCooldown() then return end
+
+    if not token then
+        token = GetToken()
+        if not token then return warn(("This TriggerEvent '%s' can not be played"):format(name)) end
     end
 
-    name = ("__%s__:%s:%s:%s"):format('supv', token, supv.service, name)
-    
+    name = ("__%s__:%s:%s:%s"):format('supv', token, supv.service,name)
     TriggerEvent(name, ...)
 end
 
@@ -59,11 +107,14 @@ end
 function supv.triggerServer(name, ...)
     if type(name) ~= 'string' then return end
 
-    while not token do
-        Wait(500)
+    local event = IsEventCooldown(name)
+    if event and event:onCooldown() then return end
+
+    if not token then
+        token = GetToken()
+        if not token then return warn(("This TriggerServerEvent '%s' can not be played"):format(name)) end
     end
 
     name = ("__%s__:%s:%s:%s"):format('supv', token, 'server', name)
-    
     TriggerServerEvent(name, ...)
 end
