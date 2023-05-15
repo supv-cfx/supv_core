9cb1faf8199bbb099bac9e0122d05e1be9a52253
SUP2Ak <cormier.wesley@gmail.com>
2023-05-15 13:59:41 +0200

    refactor(web/modal): rewrite component

--- web/src/features/modal/components/buttons.tsx
+++ web/src/features/modal/components/buttons.tsx
@@ -4,33 +4,43 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { IconProp } from '@fortawesome/fontawesome-svg-core';
 
 interface Props {
-    iconAwesome: IconProp;
-    text: string;
-    onClick: Function;
-    color: string;
-    args: boolean;
+  iconAwesome: IconProp;
+  text: string;
+  onClick: Function;
+  color: string;
+  args: boolean;
+  isDisabled?: boolean;
 }
 
-const AnimatedButton: React.FC<Props> = ({iconAwesome, text, onClick, color, args}) => {
-    const [isHovered, setIsHovered] = useState(false);
+const AnimatedButton: React.FC<Props> = ({
+  iconAwesome,
+  text,
+  onClick,
+  color,
+  args,
+  isDisabled
+}) => {
+  const [isHovered, setIsHovered] = useState(false);
 
-    return (
-        <Button
-            onMouseEnter={() => setIsHovered(true)}
-            onMouseLeave={() => setIsHovered(false)}
-            rightIcon={
-                <FontAwesomeIcon
-                    icon={iconAwesome}
-                    shake={isHovered}
-                    style={{ transition: "transform 0.5s" }}
-                />
-            }
-            color={color}
-            onClick={() => onClick(args)}
-        >
-            {text}
-        </Button>
-    );
-}
+  return (
+    <Button
+      disabled={isDisabled}
+      size='xs'
+      onMouseEnter={() => setIsHovered(true)}
+      onMouseLeave={() => setIsHovered(false)}
+      rightIcon={
+        <FontAwesomeIcon
+          icon={iconAwesome}
+          shake={isHovered}
+          style={{ transition: 'transform 0.5s' }}
+        />
+      }
+      color={color}
+      onClick={() => onClick(args)}
+    >
+      {text}
+    </Button>
+  );
+};
 
 export default AnimatedButton;
