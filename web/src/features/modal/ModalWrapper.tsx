32203029be53e6999bbccd2e4e91c7e3ea0d0a84
SUP2Ak <cormier.wesley@gmail.com>
2023-05-15 13:57:46 +0200

    refactor(web/modal): promise on wrapper

--- web/src/features/modal/ModalWrapper.tsx
+++ web/src/features/modal/ModalWrapper.tsx
@@ -1,33 +1,55 @@
-import React, { useState } from "react";
-import { useNuiEvent } from "../../hooks/useNuiEvent";
-import { useDisclosure } from "@mantine/hooks";
-import OpenModalConfirm from "./ModalConfirm";
+import React, { useState } from 'react';
+import { useNuiEvent } from '../../hooks/useNuiEvent';
+import { useDisclosure } from '@mantine/hooks';
+//import { createStyles } from "@mantine/core";
+import type { ModalProps } from '../../typings';
+import { useConfig } from '../../providers/ConfigProvider';
 
-import type { ModalProps } from "../../typings";
+import { OpenModalConfirm, OpenModalCustom } from './modals';
 
 //import { fetchNui } from "../../utils/fetchNui";
 
 const ModalWrapper: React.FC = () => {
-    const [data, setData] = useState<ModalProps>({type: ""});
+  const { config } = useConfig();
+  //const useStyles = createStyles((theme) => ({...config.modalsStyles}));
+  //const { classes } = useStyles();
+  const [data, setData] = useState<ModalProps>({ type: '' });
+  const [opened, { close, open }] = useDisclosure(false);
 
-    const [opened, { close, open }] = useDisclosure(false);
+  useNuiEvent('supv:modal:opened', async (data) => {
+    if (
+      data.type === 'confirm' &&
+      !data.description &&
+      !data.title &&
+      !data.subtitle
+    )
+      return;
+    setData(data);
+    await new Promise((resolve) => setTimeout(resolve, 200));
+    open();
+  });
 
-    useNuiEvent("supv:modal:opened", (data) => {
-        if (data.type === "confirm" && !data.description && !data.title && !data.subtitle) return;
-
-        setData(data);
-        open();
-    });
-
-    return (
-        <>
-            {opened && (
-                data.type === "confirm" ? 
-                    <OpenModalConfirm title={data.title} subtitle={data.subtitle} description={data.description} handleClose={close} />
-                : null
-            )}
-        </>
-    );
-}
+  return (
+    <>
+      {data.type === 'custom' && (
+        <OpenModalCustom
+          title={data.title}
+          options={data.options}
+          handleClose={close}
+          open={opened && data.type === 'custom'}
+        />
+      )}
+      {data.type === 'confirm' && (
+        <OpenModalConfirm
+          title={data.title}
+          subtitle={data.subtitle}
+          description={data.description}
+          handleClose={close}
+          open={opened && data.type === 'confirm'}
+        />
+      )}
+    </>
+  );
+};
 
 export default ModalWrapper;
