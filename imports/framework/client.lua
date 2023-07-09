412febbc499274045363c4bdeb069f44b2c7aaa4
SUP2Ak <cormier.wesley@gmail.com>
2023-07-09 23:22:23 +0200

    feat(imports): add framework bridge

---
+++ imports/framework/client.lua
@@ -0,0 +1,17 @@
+local GetResourceState <const> = GetResourceState
+local core <const> = (GetResourceState('es_extended') ~= 'missing' and 'esx') or (GetResourceState('qb-core') ~= 'missing' and 'qbcore')
+
+local function import(framework)
+    local file <const> = ('imports/framework/%s/client.lua'):format(framework)
+    local chunk, err = load(LoadResourceFile('supv_core', file), ('@@supv_core/%s'):format(file))
+
+    if err or not chunk then
+        error(err or ("Unable to load file of framework '%s'"):format(file))
+    end
+
+    return chunk()
+end
+
+if core then
+   return import(core) 
+end
