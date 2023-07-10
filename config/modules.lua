e2c68c599325ed12a1c9a64a7be9d40b4e3932b7
SUP2Ak <cormier.wesley@gmail.com>
2023-07-10 19:46:13 +0200

    feat(condig): add modules path

---
+++ config/modules.lua
@@ -0,0 +1,8 @@
+return {
+    -- 'mysql', -- init mysql
+    'handlers', -- init handlers server & client
+    'nui', -- init nui server & client
+    'main', -- init main server & client
+    -- 'richpresence', -- init rich-presence
+    -- 'setting'
+}
