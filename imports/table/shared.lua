5156d842b0e5fd491dd886dd5d6fdd6a81140e75
SUP2Ak <cormier.wesley@gmail.com>
2023-07-11 00:09:20 +0200

    feat(shared\imports.table): toVec3 & toVec4 converter

--- imports/table/shared.lua
+++ imports/table/shared.lua
@@ -31,7 +31,40 @@ local function Contains(t, value)
 	return false
 end
 
+local function toVec3(coords)
+    local _type <const> = type(coords)
+
+    if _type ~= 'vector3' then
+        if _type == 'table' or _type == 'vector4' then
+            return vec3(coords[1] or coords.x, coords[2] or coords.y, coords[3] or coords.z)
+        end
+
+        error(("expected type 'vector3' or 'table' (received %s)"):format(_type))
+    end
+
+    return coords
+end
+
+---@param coords vector3|table
+---@param heading? number
+---@return vector4
+local function toVec4(coords, heading)
+	local _type <const> = type(coords)
+
+	if _type ~= 'vector4' then
+		if _type == 'table' or _type == 'vector3' then
+			return vec4(coords[1] or coords.x, coords[2] or coords.y, coords[3] or coords.z, coords[4] or coords.w or coords.h or heading)
+		end
+
+		error(("expected type 'vector4' or 'table' (received %s)"):format(_type))
+	end
+
+	return coords
+end
+
 return {
     clone = Clone,
 	contains = Contains
+	toVec3 = toVec3,
+	toVec4 = toVec4
 }
