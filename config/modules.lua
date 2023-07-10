0501dda0cc0601942592f24732eef6b562c7a592
SUP2Ak <cormier.wesley@gmail.com>
2023-07-10 23:30:20 +0200

    tweak(config.modules): remove commentary

--- config/modules.lua
+++ config/modules.lua
@@ -1,8 +1,5 @@
 return {
-    -- 'mysql', -- init mysql
     'handlers', -- init handlers server & client
     'nui', -- init nui server & client
     'main', -- init main server & client
-    -- 'richpresence', -- init rich-presence
-    -- 'setting'
 }
