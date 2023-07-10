13e48b040a6e3a53a827c9ef33bddff2df0c794b
SUP2Ak <cormier.wesley@gmail.com>
2023-07-10 19:35:17 +0200

    feat(imports\string): add UUID generator

--- imports/string/shared.lua
+++ imports/string/shared.lua
@@ -46,8 +46,17 @@ local function RandomString(length, data)
     return output
 end
 
+local function GenerateUUID()
+    local template ='xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'
+    return template:gsub('[xy]', function(repl)
+        local v = (repl == 'x') and math.random(0, 0xf) or math.random(8, 0xb)
+        return ('%x'):format(v)
+    end)
+end
+
 return {
     starts = Starts,
     random = RandomString,
     firstToUpper = FirstToUpper
+    uuid = GenerateUUID
 }
