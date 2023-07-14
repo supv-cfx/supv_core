7145eb9eeb5ba56707825fdc35b454357af39c9b
SUP2Ak <cormier.wesley@gmail.com>
2023-07-14 23:11:40 +0200

    feat(nui): add config (wip)

---
+++ modules/nui/client/config.lua
@@ -0,0 +1,30 @@
+---@todo: more implementation about config interface from .cfg file
+
+-- local p <const> = require 'imports.promise.shared'
+local config <const> = {
+    notificationStyles = json.decode(GetConvar('supv_core:interface:notification:simple', ''))
+}
+
+supv.registerReactCallback('supv:react:getConfig', function(data, cb)
+    Wait(500)
+    supv.notify('simple', {
+        id = 'init_nui',
+        type = 'success',
+        description = 'supv interface initialized',
+    })
+end)
+
+--[[
+p.new(function(resolve, reject)
+    local playerId = supv.getCache('playerid')
+    while not playerId do
+        playerId = supv.getCache('playerid')
+        Wait(500)
+    end
+    resolve()
+end):Then(function(result)
+    supv.sendReactMessage(false, {
+        action = 'supv:react:getConfig',
+        data = config
+    })
+end) ]]
--- modules/nui/index.lua
+++ modules/nui/index.lua
@@ -1,5 +1,6 @@
 return {
     client = {
+        'config',
         'modals',
         'notify',
     },
