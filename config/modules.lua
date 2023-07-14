04ca5b92bb2b1557d4a5c31b6d4e7dc339efdda9
SUP2Ak <cormier.wesley@gmail.com>
2023-07-14 23:11:15 +0200

    refactor(config\modules): re order

--- config/modules.lua
+++ config/modules.lua
@@ -1,5 +1,5 @@
 return {
     'handlers', -- init handlers server & client
-    'nui', -- init nui server & client
     'main', -- init main server & client
+    'nui', -- init nui server & client
 }
