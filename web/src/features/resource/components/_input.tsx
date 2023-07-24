e6bd2d4114e3f2623f7025501d84905de85734ac
SUP2Ak <cormier.wesley@gmail.com>
2023-07-24 22:40:10 +0200

    feat(web\resource): input editor components

---
+++ web/src/features/resource/components/_input.tsx
@@ -0,0 +1,48 @@
+import React, { useState } from "react";
+import { TextInput, Box } from "@mantine/core";
+import { _ButtonsEditor } from "./_buttons";
+import { _StringEditorProps } from "../../../typings";
+
+export const InputEdit: React.FC<_StringEditorProps> = ({
+	inputKey,
+	label,
+	description,
+	placeholder,
+	defaultValue,
+	currentValue,
+	resource,
+	file,
+}) => {
+	const [isDisabled, setIsDisabled] = useState<boolean>(true);
+  const [value, setValue] = useState<string | undefined>(currentValue || defaultValue);
+	
+	return (
+		<Box
+			sx={{
+				backgroundColor: "rgba(255,255,255,0.035)",
+				outline: "solid rgba(255,255,255, 0.2)",
+				borderRadius: 2,
+				margin: 5,
+			}}
+		>
+			<TextInput
+				key={inputKey}
+				label={label}
+				placeholder={placeholder}
+				defaultValue={currentValue || defaultValue}
+				description={description}
+				disabled={isDisabled}
+        onChange={(e) => {setValue(e.currentTarget.value)}}
+				m={5}
+			/>
+      <_ButtonsEditor 
+        inputKey={inputKey}
+        resource={resource}
+        file={file}
+        value={value}
+        setIsDisabled={setIsDisabled}
+        isDisabled={isDisabled}
+      />
+		</Box>
+	);
+};
