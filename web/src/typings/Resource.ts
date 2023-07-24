7bc429cd23ff663dcc4474eb32c6569069b43354
SUP2Ak <cormier.wesley@gmail.com>
2023-07-24 22:40:57 +0200

    feat(web\typings): add resource

---
+++ web/src/typings/Resource.ts
@@ -0,0 +1,28 @@
+export interface _BooleanSwitchProps {
+	inputKey: string;
+	label: string;
+	description?: string;
+  currentValue: boolean;
+  resource: string;
+  file: string;
+}
+
+export interface _ButtonEditorProps {
+  inputKey: string;
+  value?: any;
+  resource: string;
+  file: string;
+  isDisabled: boolean;
+  setIsDisabled: (value: boolean) => void;
+}
+
+export interface _StringEditorProps {
+	inputKey: string;
+	label: string;
+	description?: string;
+	placeholder?: string;
+	defaultValue?: string;
+	currentValue?: string;
+	resource: string;
+	file: string;
+}
--- web/src/typings/index.ts
+++ web/src/typings/index.ts
@@ -4,6 +4,7 @@ export * from './Notification';
 export * from './ConvertUnix';
 export * from './InputDate';
 export * from './Modal';
+export * from './Resource';
 
 // configuration
 export * from './config/notifications';
