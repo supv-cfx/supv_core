4ecdb5d307f5b0b0b35423e52c11b5b583b132e5
SUP2Ak <cormier.wesley@gmail.com>
2023-07-31 23:45:04 +0200

    tweak(web\app) space

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -42,7 +42,7 @@ const App: React.FC = () => {
 					<ModalConfirm />
 					<NotificationsWrapper />
 					<ResourceManager />
-					{isEnvBrowser() && <DevTool />}
+				{isEnvBrowser() && <DevTool />}
 				</MantineProvider>
 			</ColorSchemeProvider>
 		</>
