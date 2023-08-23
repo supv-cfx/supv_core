c1a3424548723060d2d3e5fb8c50529b94fd28c4
SUP2Ak <cormier.wesley@gmail.com>
2023-08-23 23:37:09 +0200

    refactor(on): remove cooldown method

--- imports/on/client.lua
+++ imports/on/client.lua
@@ -1,43 +1,18 @@
 local RegisterNetEvent <const>, AddEventHandler <const> = RegisterNetEvent, AddEventHandler
 
-local function EventHandler(_, name, cb, cooldown, ...)
-    if type(name) ~= 'string' then return end
-    if cb and (type(cb) ~= 'table' and type(cb) ~= 'function') then return end
-
-    if type(cooldown) == 'number' then
-        supv.RegisterEventCooldown(name, cooldown)
-    end
-
-    local eventHandler = function(...)
-        local eventCooldown <const> = cooldown and supv.IsEventCooldown(name)
-        if eventCooldown and eventCooldown:onCooldown() then
-            return warn('Ignoring event', name, 'because of cooldown'..'\n')
-        end
-        cb(...)
-    end
-
-    return AddEventHandler(supv:hashEvent(name), eventHandler)
+local function addEventHandler(_, name, cb)
+    AddEventHandler(supv:hashEvent(name), cb)
 end
 
 supv.on = setmetatable({}, {
-    __call = EventHandler
+    __call = addEventHandler
 })
 
-function supv.on.net(name, cb, cooldown)
+function supv.on.net(name, cb)
     if type(name) ~= 'string' then return end
     if cb and (type(cb) ~= 'table' and type(cb) ~= 'function') then return RegisterNetEvent(supv:hashEvent(name)) end
 
-    if type(cooldown) == 'number' then
-        supv.RegisterEventCooldown(name, cooldown)
-    end
-
-    local eventHandler = function(...)
-        local eventCooldown <const> = cooldown and supv.IsEventCooldown(name)
-        if eventCooldown and eventCooldown:onCooldown() then
-            return warn('Ignoring event', name, 'because of cooldown'..'\n')
-        end
-        cb(...)
-    end
-
-    return RegisterNetEvent(self:hashEvent(name), eventHandler)
+    RegisterNetEvent(supv:hashEvent(name), cb)
 end
+
+return supv.on
--- imports/on/server.lua
+++ imports/on/server.lua
@@ -1,30 +1,15 @@
 local RegisterNetEvent <const>, AddEventHandler <const> = RegisterNetEvent, AddEventHandler
 
-local function EventHandler(name, token, source, cb, cooldown, global, ...)
+local function EventHandler(name, token, source, cb, ...)
     if (source and source ~= '') and (not token or token ~= supv.token) then return warn(("This player id : %s have execute event %s without token! (identifier: %s)"):format(source, name, supv.getIdentifierFromId(source, 'license'))) end
-    if cooldown then
-        local eventCooldown <const> = cooldown and supv.IsEventCooldown(name, global or source)
-        if eventCooldown and eventCooldown:onCooldown then
-            return global and warn('Ignoring event : '..name, 'because of global cooldown'..'\n') or warn('Ignoring event', name, 'because of cooldown for source : '..source..'\n')
-        end
-        supv.RegisterEventCooldown(name, cooldown, global)
-    end
     cb(source, ...)
 end
 
-local function PlayHandler(_, name, cb, cooldown)
+local function PlayHandler(_, name, cb)
     if type(name) ~= 'string' then return end
     if cb and (type(cb) ~= 'table' and type(cb) ~= 'function') then return end
 
-    if type(cooldown) == 'number' then
-        supv.RegisterEventCooldown(name, cooldown)
-    end
-
     local eventHandler = function(token, ...)
-        local eventCooldown <const> = cooldown and supv.IsEventCooldown(name, source)
-        if eventCooldown and eventCooldown:onCooldown then
-            return warn('Ignoring event', name, 'because of cooldown for source : '..source..'\n')
-        end
         cb(...)
     end
 
@@ -37,11 +22,13 @@ supv.on = setmetatable({}, {
 
 function supv.on.net(name, cb, cooldown, global)
     if type(name) ~= 'string' then return end
-    if cb and (type(cb) ~= 'table' and type(cb) ~= 'function') then return RegisterNetEvent(supv:hashEvent(name)) end
+    if cb and (type(cb) ~= 'table' and type(cb) ~= 'function') then return RegisterNetEvent(supv.hashEvent(supv, name)) end
 
     local eventHandler = function(token, ...)
-        return EventHandler(name, token, source, cb, cooldown, global, ...)
+        EventHandler(name, token, source, cb, ...)
     end
 
     return RegisterNetEvent(supv:hashEvent(name), eventHandler)
 end
+
+return supv.on
