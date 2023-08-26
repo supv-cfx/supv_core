2f751e27f4ce8ea114b6e6bb22cf06671b32a450
SUP2Ak <cormier.wesley@gmail.com>
2023-08-26 02:33:18 +0200

    fix(server/on): a lot of fix

--- imports/on/server.lua
+++ imports/on/server.lua
@@ -1,7 +1,10 @@
 local RegisterNetEvent <const>, AddEventHandler <const> = RegisterNetEvent, AddEventHandler
+local GetPlayerIdentifierByType <const> = GetPlayerIdentifierByType
+
+local Token <const> = supv.getToken()
 
 local function EventHandler(name, token, source, cb, ...)
-    if (source and source ~= '') and (not token or token ~= supv.token) then return warn(("This player id : %s have execute event %s without token! (identifier: %s)"):format(source, name, supv.getIdentifierFromId(source, 'license'))) end
+    if (source and source ~= '') and (not token or token ~= Token) then return warn(("This player id : %s have execute event %s without token! (identifier: %s)"):format(source, name, GetPlayerIdentifierByType(source, 'license'))) end
     cb(source, ...)
 end
 
@@ -13,7 +16,7 @@ local function PlayHandler(_, name, cb)
         cb(...)
     end
 
-    return AddEventHandler(supv:hashEvent(name), eventHandler)
+    AddEventHandler(supv:hashEvent(name), eventHandler)
 end
 
 supv.on = setmetatable({}, {
@@ -22,13 +25,13 @@ supv.on = setmetatable({}, {
 
 function supv.on.net(name, cb, cooldown, global)
     if type(name) ~= 'string' then return end
-    if cb and (type(cb) ~= 'table' and type(cb) ~= 'function') then return RegisterNetEvent(supv.hashEvent(supv, name)) end
+    if cb and (type(cb) ~= 'table' and type(cb) ~= 'function') then return RegisterNetEvent(supv:hashEvent(name)) end
 
     local eventHandler = function(token, ...)
         EventHandler(name, token, source, cb, ...)
     end
 
-    return RegisterNetEvent(supv:hashEvent(name), eventHandler)
+    RegisterNetEvent(supv:hashEvent(name), eventHandler)
 end
 
 return supv.on
