29f3f9ec2fbaf1e69fef9ef92610cb174a816026
SUP2Ak <cormier.wesley@gmail.com>
2023-09-10 01:42:41 +0200

    tweak(web/modal._dateinput): set 'fr' dayjs locale

--- web/src/features/modal/components/custom/_dateInput.tsx
+++ web/src/features/modal/components/custom/_dateInput.tsx
@@ -1,48 +1,57 @@
-import React, { useState } from 'react';
-import { DateInput } from '@mantine/dates';
-import type { _DateInputProps } from '../../../../typings';
-import dayjs from 'dayjs';
+import React, { useState } from "react";
+import { DateInput } from "@mantine/dates";
+import type { _DateInputProps } from "../../../../typings";
+import dayjs from "dayjs";
+import "dayjs/locale/fr";
+//import { fr } from 'date-fns/locale';
 
 interface DateType {
-  status: 'subtract' | 'add';
-  type: 'year' | 'month' | 'day';
-  value: number;
+	status: "subtract" | "add";
+	type: "year" | "month" | "day";
+	value: number;
 }
 
-export const DateInputField: React.FC<_DateInputProps> = ({index, label, data, onChanged, props}) => {
-  const [value, setValue] = useState<Date | null>(null);
+export const DateInputField: React.FC<_DateInputProps> = ({
+	index,
+	label,
+	data,
+	onChanged,
+	props,
+}) => {
+	const [value, setValue] = useState<Date | null>(null);
 
-  const formatDate = (date: Date | null): string => {
-    return date
-      ? date.toLocaleDateString('fr-FR', {
-          day: '2-digit',
-          month: '2-digit',
-          year: 'numeric',
-        })
-      : '';
-  };
+	const formatDate = (date: Date | null): string => {
+		return date
+			? date.toLocaleDateString("fr-FR", {
+					day: "2-digit",
+					month: "2-digit",
+					year: "numeric",
+			  })
+			: "";
+	};
 
-  const handlerChange = (date: Date | null) => {
-    setValue(date);
-    onChanged(index, formatDate(date), data?.required, data?.callback);
-  };
+	const handlerChange = (date: Date | null) => {
+		setValue(date);
+		onChanged(index, formatDate(date), data?.required, data?.callback);
+	};
 
-  const onPlaceHolder = (date: Date | null): string => {
-    return date
-      ? formatDate(date)
-      : new Date().toLocaleDateString('fr-FR', {
-          day: '2-digit',
-          month: '2-digit',
-          year: 'numeric',
-        });
-  };
+	const onPlaceHolder = (date: Date | null): string => {
+		return date
+			? formatDate(date)
+			: new Date().toLocaleDateString("fr-FR", {
+					day: "2-digit",
+					month: "2-digit",
+					year: "numeric",
+			  });
+	};
 
-  const selectOnDate = (data: DateType) => {
-    return dayjs(new Date())[data.status](data?.value, data?.type).toDate();
-  }
+	const selectOnDate = (data: DateType) => {
+		return dayjs(new Date())[data.status](data?.value, data?.type).toDate();
+	};
 
-  return (
+	return (
 		<DateInput
+			locale="fr"
 			value={value}
 			sx={{
 				paddingTop: "10px",
@@ -79,42 +88,42 @@ export const DateInputField: React.FC<_DateInputProps> = ({index, label, data, o
 					},
 					//color: theme.colors.gray[1],
 				},
-        month: {
-          "&[data-selected]": {
-            color: "white",
-            background: "rgba(14, 44, 100, 0.86)", //theme.colors.blue[6],
-            "&, &:hover": {
-              color: "white",
-              background: "rgba(14, 44, 100, 0.86)",
-            },
-          },
-          "&:hover": {
-            //color: theme.colors.blue[8],
-            //backgroundColor: "rgba(14, 44, 100, 0.86)",
-            //color: "white",
-          },
-          //color: theme.colors.gray[1],
-        },
-        yearsList: {
-          "&[data-selected]": {
-            color: "white",
-            background: "rgba(14, 44, 100, 0.86)", //theme.colors.blue[6],
-            "&, &:hover": {
-              color: "white",
-              background: "rgba(14, 44, 100, 0.86)",
-            },
-          },
-          '&[data-hovered]': {
-            //color: theme.colors.blue[8],
-            background: 'rgba(14, 44, 100, 0.86)',
-          },
-          "&:hover": {
-            //color: theme.colors.blue[8],
-            //backgroundColor: "rgba(14, 44, 100, 0.86)",
-            //color: "white",
-          },
-          //color: theme.colors.gray[1],
-        },
+				month: {
+					"&[data-selected]": {
+						color: "white",
+						background: "rgba(14, 44, 100, 0.86)", //theme.colors.blue[6],
+						"&, &:hover": {
+							color: "white",
+							background: "rgba(14, 44, 100, 0.86)",
+						},
+					},
+					"&:hover": {
+						//color: theme.colors.blue[8],
+						//backgroundColor: "rgba(14, 44, 100, 0.86)",
+						//color: "white",
+					},
+					//color: theme.colors.gray[1],
+				},
+				yearsList: {
+					"&[data-selected]": {
+						color: "white",
+						background: "rgba(14, 44, 100, 0.86)", //theme.colors.blue[6],
+						"&, &:hover": {
+							color: "white",
+							background: "rgba(14, 44, 100, 0.86)",
+						},
+					},
+					"&[data-hovered]": {
+						//color: theme.colors.blue[8],
+						background: "rgba(14, 44, 100, 0.86)",
+					},
+					"&:hover": {
+						//color: theme.colors.blue[8],
+						//backgroundColor: "rgba(14, 44, 100, 0.86)",
+						//color: "white",
+					},
+					//color: theme.colors.gray[1],
+				},
 			})}
 			popoverProps={{ withinPortal: true, position: "top" }}
 			minDate={data.minDate && selectOnDate(data.minDate)}
