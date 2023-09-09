419d6c85b92d46d775693f9c7277227e0428fd7d
SUP2Ak <cormier.wesley@gmail.com>
2023-09-10 00:42:42 +0200

    feat(wev/modal._date): support min & max data selectable

--- web/src/features/modal/components/custom/_dateInput.tsx
+++ web/src/features/modal/components/custom/_dateInput.tsx
@@ -1,6 +1,13 @@
 import React, { useState } from 'react';
 import { DateInput } from '@mantine/dates';
 import type { _DateInputProps } from '../../../../typings';
+import dayjs from 'dayjs';
+
+interface DateType {
+  status: 'subtract' | 'add';
+  type: 'year' | 'month' | 'day';
+  value: number;
+}
 
 export const DateInputField: React.FC<_DateInputProps> = ({index, label, data, onChanged, props}) => {
   const [value, setValue] = useState<Date | null>(null);
@@ -30,19 +37,96 @@ export const DateInputField: React.FC<_DateInputProps> = ({index, label, data, o
         });
   };
 
+  const selectOnDate = (data: DateType) => {
+    return dayjs(new Date())[data.status](data?.value, data?.type).toDate();
+  }
+
   return (
-      <DateInput
-        value={value}
-        sx={{ paddingTop: '10px' }}
-        popoverProps={{ withinPortal: true, position: 'top' }}
-        onChange={handlerChange}
-        label={label}
-        placeholder={onPlaceHolder(value)}
-        valueFormat='DD/MM/YYYY'
-        mx='xs'
-        required={data?.required || false}
-        error={props.error || false}
-        withAsterisk={data?.required || false}
-      />
-  );
+		<DateInput
+			value={value}
+			sx={{
+				paddingTop: "10px",
+				//item: {
+				//  '&[data-selected]': {
+				//    color: 'white',
+				//    background: 'rgba(14, 44, 100, 0.86)', //theme.colors.blue[6],
+				//    '&, &:hover': {
+				//      color: 'white',
+				//      background: 'rgba(14, 44, 100, 0.86)'
+				//    },
+				//  },
+				//  '&[data-hovered]': {
+				//    //color: theme.colors.blue[8],
+				//    background: 'rgba(14, 44, 100, 0.86)',
+				//  },
+				//  //color: theme.colors.gray[1],
+				//},
+			}}
+			styles={(theme) => ({
+				day: {
+					"&[data-selected]": {
+						color: "white",
+						background: "rgba(14, 44, 100, 0.86)", //theme.colors.blue[6],
+						"&, &:hover": {
+							color: "white",
+							background: "rgba(14, 44, 100, 0.86)",
+						},
+					},
+					"&:hover": {
+						//color: theme.colors.blue[8],
+						backgroundColor: "rgba(14, 44, 100, 0.86)",
+						color: "white",
+					},
+					//color: theme.colors.gray[1],
+				},
+        month: {
+          "&[data-selected]": {
+            color: "white",
+            background: "rgba(14, 44, 100, 0.86)", //theme.colors.blue[6],
+            "&, &:hover": {
+              color: "white",
+              background: "rgba(14, 44, 100, 0.86)",
+            },
+          },
+          "&:hover": {
+            //color: theme.colors.blue[8],
+            //backgroundColor: "rgba(14, 44, 100, 0.86)",
+            //color: "white",
+          },
+          //color: theme.colors.gray[1],
+        },
+        yearsList: {
+          "&[data-selected]": {
+            color: "white",
+            background: "rgba(14, 44, 100, 0.86)", //theme.colors.blue[6],
+            "&, &:hover": {
+              color: "white",
+              background: "rgba(14, 44, 100, 0.86)",
+            },
+          },
+          '&[data-hovered]': {
+            //color: theme.colors.blue[8],
+            background: 'rgba(14, 44, 100, 0.86)',
+          },
+          "&:hover": {
+            //color: theme.colors.blue[8],
+            //backgroundColor: "rgba(14, 44, 100, 0.86)",
+            //color: "white",
+          },
+          //color: theme.colors.gray[1],
+        },
+			})}
+			popoverProps={{ withinPortal: true, position: "top" }}
+			minDate={data.minDate && selectOnDate(data.minDate)}
+			maxDate={data.maxDate && selectOnDate(data.maxDate)}
+			onChange={handlerChange}
+			label={label}
+			placeholder={onPlaceHolder(value)}
+			valueFormat="DD/MM/YYYY"
+			mx="xs"
+			required={data?.required || false}
+			error={props.error || false}
+			withAsterisk={data?.required || false}
+		/>
+	);
 };
