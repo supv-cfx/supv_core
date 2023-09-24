f0ad5c2fc3b17af9950ad6b2553d9be9fc67397c
SUP2Ak <cormier.wesley@gmail.com>
2023-09-24 18:32:27 +0200

    feat(imports): add area module

---
+++ imports/area/client.lua
@@ -0,0 +1,32 @@
+local GetGamePool <const> = GetGamePool
+local GetEntityCoords <const> = GetEntityCoords
+
+local function GetVehicleInArea(coords, maxDistance, inside)
+    local vehicles = GetGamePool('CVehicle')
+    local closestVehicle, closestCoords
+    maxDistance = maxDistance or 2
+    local vehiclesFound = {}
+
+    for i = 1, #vehicles do
+        local vehicle = vehicles[i]
+
+        if not supv.cache.vehicle or supv.cache.vehicle ~= vehicle or inside then
+            local vehicleCoords = GetEntityCoords(vehicle)
+            local distance = #(coords - vehicleCoords)
+
+            if distance < maxDistance then
+                vehiclesFound[#vehiclesFound + 1] = {
+                    vehicle = vehicle,
+                    coords = vehicleCoords,
+                    distance = distance
+                }
+            end
+        end
+    end
+
+    return #vehiclesFound > 0 and vehiclesFound or false
+end
+
+return {
+    vehicles = GetVehicleInArea
+}
