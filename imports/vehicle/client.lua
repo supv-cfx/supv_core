d44feff1a1e485fedbfdc4c6a934371735068790
SUP2Ak <cormier.wesley@gmail.com>
2023-11-26 18:43:48 +0100

    feat(imports/vehicle.client): spawn method

--- imports/vehicle/client.lua
+++ imports/vehicle/client.lua
@@ -5,21 +5,35 @@ local function RemoveVehicle(self)
     return nil, collectgarbage()
 end
 
+local function Edit(self, data)
+    if DoesEntityExist(self.vehicle) then
+        if data.plate then SetVehicleNumberPlateText(self.vehicle, data.plate) end
+        if data.alpha then SetEntityAlpha(self.vehicle, data.alpha[1], data.alpha[2]) end
+        if data.ground then SetVehicleOnGroundProperly(self.vehicle) end
+        if data.coords then 
+            SetEntityCoords(self.vehicle, data.coords.x, data.coords.y, data.coords.z)
+            SetEntityHeading(self.vehicle, data.coords.w) 
+        end
+    end
+end
+
 local function SpawnVehicle(model, coords, data)
     local self, p = {}, promise.new()
 
     self.model = type(model) == 'number' and model or joaat(model)
     self.coords = coords
-    self.data = data or {}
+    self.data = data
 
     self.remove = RemoveVehicle
+    self.edit = Edit
 
     CreateThread(function()
         for i = 1, 10 do
+            RequestModel(self.model)
+
             if HasModelLoaded(self.model) then
                 break
             end
-            RequestModel(self.model)
             Wait(100)
         end
 
@@ -28,6 +42,10 @@ local function SpawnVehicle(model, coords, data)
         ---@todo: add vehicle data setter
 
         if DoesEntityExist(self.vehicle) then
+            if self.data?.plate then SetVehicleNumberPlateText(self.vehicle, self.data.plate) else self.data.plate = GetVehicleNumberPlateText(self.vehicle) end
+            if self.data?.alpha then SetEntityAlpha(self.vehicle, self.data.alpha[1], self.data.alpha[2]) end
+            if self.data?.ground then SetVehicleOnGroundProperly(self.vehicle) end
+            SetModelAsNoLongerNeeded(self.model)
             p:resolve(self)
         else
             p:reject(('unable to spawn vehicle %s'):format(model))
@@ -36,3 +54,7 @@ local function SpawnVehicle(model, coords, data)
 
     return supv.await(p)
 end
+
+return {
+    spawn = SpawnVehicle,
+}
