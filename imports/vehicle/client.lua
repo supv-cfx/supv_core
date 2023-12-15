ed2a985c9c66d6fb7798cb1e397ae8c9ac1a225a
SUP2Ak <cormier.wesley@gmail.com>
2023-12-15 16:09:49 +0100

    refactor(imports/vehicle.client): spawn  & method

--- imports/vehicle/client.lua
+++ imports/vehicle/client.lua
@@ -1,3 +1,15 @@
+local DoesEntityExist <const> = DoesEntityExist
+local CreateVehicle <const> = CreateVehicle
+local DeleteEntity <const> = DeleteEntity
+local SetVehicleNumberPlateText <const> = SetVehicleNumberPlateText
+local SetEntityAlpha <const> = SetEntityAlpha
+local SetVehicleOnGroundProperly <const> = SetVehicleOnGroundProperly
+local SetEntityCoords <const> = SetEntityCoords
+local SetEntityHeading <const> = SetEntityHeading
+local GetVehicleNumberPlateText <const> = GetVehicleNumberPlateText
+
+---@todo documentation
+
 local function RemoveVehicle(self)
     if DoesEntityExist(self.vehicle) then
         DeleteEntity(self.vehicle)
@@ -5,47 +17,69 @@ local function RemoveVehicle(self)
     return nil, collectgarbage()
 end
 
-local function Edit(self, data)
+---@param data any ---@todo
+---@param cb fun(self: table)
+local function Edit(self, data, cb)
     if DoesEntityExist(self.vehicle) then
-        if data.plate then SetVehicleNumberPlateText(self.vehicle, data.plate) end
-        if data.alpha then SetEntityAlpha(self.vehicle, data.alpha[1], data.alpha[2]) end
-        if data.ground then SetVehicleOnGroundProperly(self.vehicle) end
-        if data.coords then 
-            SetEntityCoords(self.vehicle, data.coords.x, data.coords.y, data.coords.z)
-            SetEntityHeading(self.vehicle, data.coords.w) 
+        if data.plate then 
+            SetVehicleNumberPlateText(self.vehicle, data.plate)
+            self.data.plate = data.plate
         end
+        if data.alpha then 
+            SetEntityAlpha(self.vehicle, data.alpha[1], data.alpha[2])
+            self.data.alpha = data.alpha
+        end
+        if data.ground then 
+            SetVehicleOnGroundProperly(self.vehicle)
+            self.data.ground = data.ground
+        end
+        if data.vec4 then
+            SetEntityCoords(self.vehicle, data.vec4.x, data.vec4.y, data.vec4.z)
+            SetEntityHeading(self.vehicle, data.vec4.w)
+            self.vec4 = data.vec4
+            self.vec3 = vec3(data.vec4.x, data.vec4.y, data.vec4.z)
+        end
+        if data.vec3 then
+            SetEntityCoords(self.vehicle, data.vec3.x, data.vec3.y, data.vec3.z)
+            self.vec3 = data.vec3
+            self.vec4 = vec4(data.vec3.x, data.vec3.y, data.vec3.z, data.vec4.w or data.vec3.h or 0.0)
+        end
+        if cb then cb(self) end
     end
 end
 
+---@param model any
+---@param coords any
+---@param data any ---@todo
+---@return table
 local function SpawnVehicle(model, coords, data)
     local self, p = {}, promise.new()
 
-    self.model = type(model) == 'number' and model or joaat(model)
-    self.coords = coords
-    self.data = data
+    self.model = model
+    self.vec3 = vec3(coords.x, coords.y, coords.z)
+    self.vec4 = vec4(coords.x, coords.y, coords.z, coords.w or coords.h or 0.0)
+    self.data = {
+        plate = data.plate,
+        alpha = data.alpha,
+        ground = data.ground,
+        network = data.network or true,
+        mission = data.mission or false,
+    }
 
     self.remove = RemoveVehicle
     self.edit = Edit
 
     CreateThread(function()
-        for i = 1, 10 do
-            RequestModel(self.model)
+        local FlushModel <const> = supv.request({ type = 'model', name = self.model })
 
-            if HasModelLoaded(self.model) then
-                break
-            end
-            Wait(100)
-        end
-
-        self.vehicle = CreateVehicle(model, coords.x, coords.y, coords.z, coords.w, self.data?.network or true, self.data?.mission or false)
-
+        self.vehicle = CreateVehicle(self.model, self.vec4.x, self.vec4.y, self.vec4.z, self.vec4.w, self.data.network, self.data.mission)
+        FlushModel(self.model)
         ---@todo: add vehicle data setter
 
         if DoesEntityExist(self.vehicle) then
-            if self.data?.plate then SetVehicleNumberPlateText(self.vehicle, self.data.plate) else self.data.plate = GetVehicleNumberPlateText(self.vehicle) end
-            if self.data?.alpha then SetEntityAlpha(self.vehicle, self.data.alpha[1], self.data.alpha[2]) end
-            if self.data?.ground then SetVehicleOnGroundProperly(self.vehicle) end
-            SetModelAsNoLongerNeeded(self.model)
+            if self.data.plate then SetVehicleNumberPlateText(self.vehicle, self.data.plate) else self.data.plate = GetVehicleNumberPlateText(self.vehicle) end
+            if self.data.alpha then SetEntityAlpha(self.vehicle, self.data.alpha[1], self.data.alpha[2]) end
+            if self.data.ground then SetVehicleOnGroundProperly(self.vehicle) end
             p:resolve(self)
         else
             p:reject(('unable to spawn vehicle %s'):format(model))
