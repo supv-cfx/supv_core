0a9b4be1edba21b0885d4a226d3dd55ce6b849fc
SUP2Ak <cormier.wesley@gmail.com>
2023-12-20 22:28:27 +0100

    tweak(web/chat): commentary code

--- web/src/features/chat/Emoji.tsx
+++ web/src/features/chat/Emoji.tsx
@@ -1,64 +1,64 @@
-import React, { useState } from 'react';
-import EmojiPicker, { Theme } from 'emoji-picker-react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faSmile } from '@fortawesome/free-regular-svg-icons';
-import { Button, useMantineColorScheme } from '@mantine/core';
-import { useConfig } from '../../providers/ConfigProvider';
-import { ConfigEmojiPicker } from '../../dev/config';
+// import React, { useState } from 'react';
+// import EmojiPicker, { Theme } from 'emoji-picker-react';
+// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+// import { faSmile } from '@fortawesome/free-regular-svg-icons';
+// import { Button, useMantineColorScheme } from '@mantine/core';
+// import { useConfig } from '../../providers/ConfigProvider';
+// import { ConfigEmojiPicker } from '../../dev/config';
 
-interface EmojiPickerProps {
-    onSelect: (emoji: Object) => void;
-}
+// interface EmojiPickerProps {
+//     onSelect: (emoji: Object) => void;
+// }
 
-const EmojiPickerButton: React.FC<EmojiPickerProps> = ({ onSelect }) => {
-    const [showEmojiPicker, setShowEmojiPicker] = useState(false);
-    const {colorScheme} = useMantineColorScheme();
-    //const { config, setConfig } = useConfig();
-    const config = {
-      emojiPicker: ConfigEmojiPicker
-    }
-    const dark = colorScheme === 'dark';
+// const EmojiPickerButton: React.FC<EmojiPickerProps> = ({ onSelect }) => {
+//     const [showEmojiPicker, setShowEmojiPicker] = useState(false);
+//     const {colorScheme} = useMantineColorScheme();
+//     //const { config, setConfig } = useConfig();
+//     const config = {
+//       emojiPicker: ConfigEmojiPicker
+//     }
+//     const dark = colorScheme === 'dark';
 
-    const handleSelectEmoji = (emoji: any) => {
-        setShowEmojiPicker(false);
-        onSelect(emoji.emoji);
-    };
+//     const handleSelectEmoji = (emoji: any) => {
+//         setShowEmojiPicker(false);
+//         onSelect(emoji.emoji);
+//     };
 
-    config.emojiPicker.theme = dark ? Theme.DARK : Theme.LIGHT;
+//     config.emojiPicker.theme = dark ? Theme.DARK : Theme.LIGHT;
     
-    let color: string = dark ? '#fff' : '#000';
-    //setConfig(config);
-    return (
-        <>
-            <div style={{ position: 'relative' }}>
-                <Button
-                    style={{
-                        width: 40,
-                        height: 40,
-                        display: 'flex',
-                        alignItems: 'center',
-                        justifyContent: 'center',
-                        backgroundColor: 'transparent',
-                    }}
-                    onClick={() => setShowEmojiPicker(!showEmojiPicker)}
-                >
-                    <FontAwesomeIcon icon={faSmile} style={{ paddingBottom: 5, color: color}} />
-                </Button>
-                {showEmojiPicker && (
-                    <div
-                        style={{
-                            position: 'absolute',
-                            top: '100%',
-                            left: 0,
-                            transform: 'translateY(10px)',
-                        }}
-                    >
-                        <EmojiPicker onEmojiClick={handleSelectEmoji} theme={config.emojiPicker.theme} {...config} />
-                    </div>
-                )}
-            </div>
-        </>
-    );
-};
+//     let color: string = dark ? '#fff' : '#000';
+//     //setConfig(config);
+//     return (
+//         <>
+//             <div style={{ position: 'relative' }}>
+//                 <Button
+//                     style={{
+//                         width: 40,
+//                         height: 40,
+//                         display: 'flex',
+//                         alignItems: 'center',
+//                         justifyContent: 'center',
+//                         backgroundColor: 'transparent',
+//                     }}
+//                     onClick={() => setShowEmojiPicker(!showEmojiPicker)}
+//                 >
+//                     <FontAwesomeIcon icon={faSmile} style={{ paddingBottom: 5, color: color}} />
+//                 </Button>
+//                 {showEmojiPicker && (
+//                     <div
+//                         style={{
+//                             position: 'absolute',
+//                             top: '100%',
+//                             left: 0,
+//                             transform: 'translateY(10px)',
+//                         }}
+//                     >
+//                         <EmojiPicker onEmojiClick={handleSelectEmoji} theme={config.emojiPicker.theme} {...config} />
+//                     </div>
+//                 )}
+//             </div>
+//         </>
+//     );
+// };
 
-export default EmojiPickerButton;
+// export default EmojiPickerButton;
