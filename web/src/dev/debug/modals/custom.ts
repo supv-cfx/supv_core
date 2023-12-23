51682f171a9da3fac6d96db0d51aca4b1eb448c8
SUP2Ak <cormier.wesley@gmail.com>
2023-12-23 13:13:58 +0100

    tweak(web/dev.custom): new value to test last fix

--- web/src/dev/debug/modals/custom.ts
+++ web/src/dev/debug/modals/custom.ts
@@ -1,7 +1,14 @@
 import { debugData } from '../../../utils/debugData';
 import type { ModalPropsCustom/*, Option */} from '../../../typings';
 const modalOptions = [
-  { type: 'input', label: 'Input Field', required: true, callback: true, error: 'Message perso', default: 'test' },
+  { type: 'input', label: 'Nom', required: true, default: 'Test', callback: true},
+  { type: 'input', label: 'Prénom', required: true, default: 'Test2', callback: true},
+  { type: 'select', label: 'Sexe', options: [{value: 'homme', label: 'Homme'}, {value: 'femme', label: 'Femme'}], required: true, default: 'homme', callback: true},
+  { type: 'number', label: 'Age', required: true, default: 10, callback: true },
+  { type: 'select', label: 'Ville', required: true, options: [{value: 'paris', label: 'Paris'}, {value: 'lyon', label: 'Lyon'}], default: 'lyon', callback: true },
+  { type: 'number', label: 'Argent', required: true, default: 10000000, format: {}, callback: true},
+  
+  /*{ type: 'input', label: 'Input Field', required: true, callback: true, error: 'Message perso', default: 'test' },
   { type: 'number', label: 'Textarea Field', required: true, callback: true, default: 10 },
   { type: 'select', label: 'Select Field', options: 
   [
