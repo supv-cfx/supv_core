4121801a33ebd3cf088740195aeeda07c6e1cc76
SUP2Ak <cormier.wesley@gmail.com>
2024-01-29 01:17:53 +0100

    refactor(notify.client): adjust notification to minimap

--- modules/nui/client/notify.lua
+++ modules/nui/client/notify.lua
@@ -1,5 +1,72 @@
-local Promise, IsNuiFocused <const> = {}, IsNuiFocused
+local Promise, IsNuiFocused <const>, hud = {}, IsNuiFocused, {}
 local on <const> = require 'imports.on.client'
+local SetScriptGfxAlign <const>, ResetScriptGfxAlign <const> = SetScriptGfxAlign, ResetScriptGfxAlign
+local GetActiveScreenResolution <const>, GetAspectRatio <const> = GetActiveScreenResolution, GetAspectRatio
+local IsRadarHidden <const>, IsBigmapActive <const>, SetBigmapActive <const> = IsRadarHidden, IsBigmapActive, SetBigmapActive
+
+---@param key string
+---@param value any
+---@param force boolean
+function hud:set(key, value, force)
+    if not force and self[key] == value then return end
+
+    self[key] = value
+    SendNUIMessage({
+        action = 'supv_core:hud:minimap',
+        data = { name = key, value = value },
+    })
+end
+
+local function MinimapToNui()
+    SetScriptGfxAlign(('L'):byte(), ('B'):byte())
+    local minimapTopX <const>, minimapTopY <const> = GetScriptGfxPosition(-0.00, 0.002 + (-0.188888))
+    local minimapBottomX <const>, minimapBottomY <const> = GetScriptGfxPosition(-0.0045 + 0.145, 0.002)
+    ResetScriptGfxAlign()
+    local w <const>, h <const> = GetActiveScreenResolution()
+    local base <const> = GetAspectRatio(true)
+    local ratio <const> = (16/9) / base
+    local x1 <const>, y1 <const> = w * minimapTopX, h * (minimapTopY + .01)
+    local x2 <const>, y2 <const> = w * minimapBottomX, h * (minimapBottomY - .02)
+    local minimapWidth <const>, minimapHeight <const> = x2 - x1, y2 - y1
+
+    return {
+        x = math.abs(x1),
+        y = math.abs(y1),
+        w = minimapWidth * ratio,
+        h = minimapHeight,
+    }
+end
+
+local function RefreshGfxToNui(force)
+    local minimap <const> = MinimapToNui()
+    for k, v in pairs(minimap) do
+        if k == 'w' then
+            local expanded <const> = IsBigmapActive()
+            local hidden <const> = IsRadarHidden()
+            if not hidden then
+                hud:set(k, expanded and v * 1.5825 or v, force)
+                hud:set('expanded', expanded, force)
+            else
+                hud:set(k, v, force)
+                hud:set('expanded', false, force)
+            end
+        elseif k == 'h' then
+            local hidden <const> = IsRadarHidden()
+            local expanded <const> = IsBigmapActive()
+            if not expanded then
+                hud:set(k, hidden and (v - v) + 16 or v, force)
+            else
+                if hidden then
+                    hud:set(k, hidden and (v - v) + 16 or v, force)
+                else
+                    hud:set(k, v * 2.5825, force)
+                end
+            end
+        else
+            hud:set(k, v, force)
+        end
+    end
+end
 
 ---@class DataPropsNotify
 ---@field id? string
@@ -7,12 +74,12 @@ local on <const> = require 'imports.on.client'
 ---@field description? string
 ---@field type? string
 ---@field style? table
----@field position? string | 'top-right' | 'bottom-right'
+---@field position? string | 'top-right' | 'bottom-right' | 'minimap'
 
 ---@param select string | 'simple' | 'advanced'
 ---@param data DataPropsNotify
 local function notify(select, data)
-    if not data.position then data.position = 'top-right' end
+    if not data.position then data.position = 'minimap' end
 
     if data.type == 'action' and supv.service == 'server' then
         return warn('You can\'t use action type in server side!')
@@ -22,16 +89,24 @@ local function notify(select, data)
 
     if select == 'simple' then
         if data.key then
+            if data.position == 'minimap' then
+                RefreshGfxToNui()
+            end
+
             supv.sendReactMessage(true, {
                 action = 'supv:notification:send',
                 data = data
             }, {
-                focus = not IsNuiFocused() and {true, false} or 'ignore',
+                focus = not IsNuiFocused() and { true, false } or 'ignore',
                 keepInput = true,
             })
             Promise[data.key] = promise.new()
             return supv.await(Promise[data.key])
         else
+            if data.position == 'minimap' then
+                RefreshGfxToNui()
+            end
+
             supv.sendReactMessage(true, {
                 action = 'supv:notification:send',
                 data = data
@@ -53,59 +128,73 @@ function supv.notifyQueue()
 end
 
 on.net('notify', notify) -- Register notify event for server
+--[[]
 
---[[RegisterCommand('notify', function()
-    supv.notify('simple', {
-        title = 'action 1',
-        description = 'Mon teste action 1!',
-        type = 'action',
-    })
+local toggle = {
+    ex = false,
+    hide = false,
+}
+RegisterCommand('min', function()
+    toggle.hide = not toggle.hide
+    DisplayRadar(not toggle.hide)
+end)
+RegisterCommand('minex', function()
+    toggle.ex = not toggle.ex
+    SetBigmapActive(toggle.ex, false)
+end)
 
-    Wait(300)
+RegisterCommand('notify', function()
+    -- supv.notify('simple', {
+    --     title = 'action 1',
+    --     description = 'Mon teste action 1!',
+    --     type = 'action',
+    -- })
 
-    supv.notify('simple', {
-        title = 'action 2',
-        description = 'Mon teste action 2!',
-        type = 'action',
-    })
+    -- Wait(300)
 
-    CreateThread(function()
-        supv.openModal('confirm', {
-            title = 'Confirmation',
-            description = 'Voulez-vous vraiment faire ça?',
-        })   
-    end)
+    -- supv.notify('simple', {
+    --     title = 'action 2',
+    --     description = 'Mon teste action 2!',
+    --     type = 'action',
+    -- })
 
-    supv.notify('simple', {
-        title = 'action 3',
-        description = 'Mon teste action 3!',
-        type = 'action',
-    })
-    supv.notify('simple', {
-        title = 'action 2',
-        description = 'Mon teste action 2!',
-        type = 'action',
-    })
-    supv.notify('simple', {
-        title = 'action 2',
-        description = 'Mon teste action 2!',
-        type = 'action',
-    })
-    supv.notify('simple', {
-        title = 'action 2',
-        description = 'Mon teste action 2!',
-        type = 'action',
-    })
-    supv.notify('simple', {
-        title = 'action 2',
-        description = 'Mon teste action 2!',
-        type = 'action',
-    })
-    supv.notify('simple', {
-        title = 'action 2',
-        description = 'Mon teste action 2!',
-        type = 'action',
-    })
+    -- CreateThread(function()
+    --     supv.openModal('confirm', {
+    --         title = 'Confirmation',
+    --         description = 'Voulez-vous vraiment faire ça?',
+    --     })   
+    -- end)
+
+    -- supv.notify('simple', {
+    --     title = 'action 3',
+    --     description = 'Mon teste action 3!',
+    --     type = 'action',
+    -- })
+    -- supv.notify('simple', {
+    --     title = 'action 2',
+    --     description = 'Mon teste action 2!',
+    --     type = 'action',
+    -- })
+    -- supv.notify('simple', {
+    --     title = 'action 2',
+    --     description = 'Mon teste action 2!',
+    --     type = 'action',
+    -- })
+    -- supv.notify('simple', {
+    --     title = 'action 2',
+    --     description = 'Mon teste action 2!',
+    --     type = 'action',
+    -- })
+    -- supv.notify('simple', {
+    --     title = 'action 2',
+    --     description = 'Mon teste action 2!',
+    --     type = 'action',
+    -- })
+    -- supv.notify('simple', {
+    --     title = 'action 2',
+    --     description = 'Mon teste action 2!',
+    --     type = 'action',
+    -- })
 
     supv.notify('simple', {
         title = 'Sans type normal',
@@ -115,7 +204,7 @@ on.net('notify', notify) -- Register notify event for server
         -- },
     })
 
-    Wait(5000)
+    Wait(3000)
 
     supv.notify('simple', {
         title = 'Warning',
@@ -124,6 +213,13 @@ on.net('notify', notify) -- Register notify event for server
         -- style = {
         --     background= 'linear-gradient(135deg, rgba(8, 25, 56, 0.94) 50%, rgba(11, 42, 100, 0.86) 100%)',
         -- },
+    })
+
+    supv.notify('simple', {
+        title = 'Loading',
+        description = 'Mon teste loading! sqdqsd sq dqsdqsd qsdqs dqsdsqdqsdqsdqsdqsdqsdqsd',
+        type = 'loading',
+        duration = 7500,
     })
 
     Wait(300)
@@ -158,4 +254,5 @@ on.net('notify', notify) -- Register notify event for server
         --     background= 'linear-gradient(135deg, rgba(8, 25, 56, 0.94) 50%, rgba(11, 42, 100, 0.86) 100%)',
         -- },
     })
-end)]]
+end)
+--]]
