2e390398af87db9537f56a25c2137059502ab01e
SUP2Ak <cormier.wesley@gmail.com>
2024-02-04 17:32:33 +0100

    feat(web/utils.misc): add MathRandom

--- web/src/utils/misc.ts
+++ web/src/utils/misc.ts
@@ -1,6 +1,13 @@
 // Will return whether the current environment is in a regular browser
 // and not CEF
-export const isEnvBrowser = (): boolean => !(window as any).invokeNative
+export const isEnvBrowser = (): boolean => !(window as any).invokeNative;
 
 // Basic no operation function
-export const noop = () => {}
+export const noop = () => {};
+
+// Math Random
+export const MathRandom = (min?: number, max?: number) => {
+  min = min || 0
+  max = max || 100
+  return Math.floor(Math.random() * (max - min + 1) + min)
+};
