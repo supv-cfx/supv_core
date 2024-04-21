9a901b59b813637b9aad1071e28ca27533aba7ce
SUP2Ak <cormier.wesley@gmail.com>
2024-04-21 13:16:43 +0200

    feat(web/utils.misc): add MathRandom & MathDigits

--- web/src/utils/misc.ts
+++ web/src/utils/misc.ts
@@ -11,3 +11,14 @@ export const MathRandom = (min?: number, max?: number) => {
   max = max || 100
   return Math.floor(Math.random() * (max - min + 1) + min)
 };
+
+// Math Round
+export const MathRound = (value: number, precision: number) => {
+  const multiplier = Math.pow(10, precision || 0);
+  return Math.round(value * multiplier) / multiplier;
+};
+
+// Math Digits
+export const MathDigits = (value: number) => {
+  return value.toString().replace(/\B(?=(\d{3})+(?!\d))/g, " ");
+};
