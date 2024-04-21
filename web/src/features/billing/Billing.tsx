ef63de8d5af2550957e489138f2df6ce726343aa
SUP2Ak <cormier.wesley@gmail.com>
2024-04-21 13:23:53 +0200

    fix(web/billing): warning compiling!

--- web/src/features/billing/Billing.tsx
+++ web/src/features/billing/Billing.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import { useDisclosure } from "@mantine/hooks";
 import { fetchNui, useNuiEvent } from "../../utils";
 import {
