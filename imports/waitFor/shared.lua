99673a373166fa214715a168ece1b57fbdfbcad8
SUP2Ak <cormier.wesley@gmail.com>
2024-05-16 00:13:14 +0200

    refactor(import.waitFor): rewrite module

--- imports/waitFor/shared.lua
+++ imports/waitFor/shared.lua
@@ -6,12 +6,18 @@ function supv.waitFor(cb, timeout, errmsg)
     local value = cb()
     if value ~= nil then return value end
 
-    timeout = timeout or 1000
-    local startTimer <const> = GetGameTimer()
+    if type(timeout) ~= 'number' then
+        timeout = 1000
+    end
+
+    local start <const> = GetGameTimer()
 
     while value == nil do Wait(0)
-        if GetGameTimer() - startTimer > timeout then
-            error(('%s (waited %.2fseconds)'):format(errmsg or 'Failed to resolve callback', (GetGameTimer() - startTimer) / 1000), 2)
+
+        local timer <const> = timeout and GetGameTimer() - start
+
+        if timer and timer > timeout then
+            error(('%s (waited %.2fseconds)'):format(errmsg or 'Failed to resolve callback', timer / 1000), 2)
         end
 
         value = cb()
