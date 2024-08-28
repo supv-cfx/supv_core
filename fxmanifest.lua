7f7a7c058432eed1d40e11ddaf43dea77a148cd9
SUP2Ak <cormier.wesley@gmail.com>
2024-08-28 22:59:27 +0200

    init: main files

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -8,27 +8,21 @@ author 'SUP2Ak'
 link 'https://github.com/SUP2Ak/supv_core'
 github 'https://github.com/SUP2Ak'
 
---ui_page 'web/build/index.html'
-
 shared_script 'init.lua'
 server_script 'modules/init.lua'
 client_script 'modules/init.lua'
 
 files {
-    'obj.lua',
-    'data/client/**.json',
-    'data/shared/**.json',
-    'config/modules.lua',
     'config/client/*.lua',
     'config/shared/*.lua',
+    'modules/starter/modules.lua',
+    'obj.lua',
     'modules/**/index.lua',
-    'modules/**/shared/**',
+    -- 'modules/**/shared/**',
     'modules/**/client/**',
-    'locales/*.json',
+    -- 'locales/*.json', -- sans doute à ajouter plus tard (du moins refaire)
     'imports/**/client.lua',
     'imports/**/shared.lua',
-    'web/build/index.html',
-    'web/build/**/*'
 }
 
 dependencies {
